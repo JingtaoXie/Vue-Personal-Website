7320268c8522fee4021811cafb7a2c6b5f280056
JingtaoXie <jxie275@aucklandnui.ac.nz>
2024-06-17 01:25:32 +0800

    First version

--- vue-personal-website/src/App.vue
+++ vue-personal-website/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header-icon"> 
+  <img alt="Vue logo" src="./assets/logo.png" class="mutual-icon">
+  <p>Toby's personal website</p>
+  </div>
+  <Homepage/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Homepage from './components/Homepage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Homepage
   }
 }
 </script>
@@ -21,6 +24,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.header-icon{
+  display: flex;
+  align-items: center;
+}
+
+.mutual-icon{
+  width:50px;
 }
 </style>
---
+++ vue-personal-website/src/components/Homepage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <img class="icon" src="../assets/Icon.png">
+    <h1>{{ title }}</h1>
+    <p>{{ introduction }}</p>
+    <div class="button-area">
+    <a href="">Educational Experience</a>
+    <a href="">Working Experience</a>
+    <a href="">Skill and Introduction</a>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Homepage',
+  data(){
+    return{
+      title: "Jingtao personal website",
+      introduction: "Welcome to my personal website, and wish that you can know me better via this website"
+    }
+  }
+}
+</script>
+
+<style>
+.icon{
+  width: 200px;
+  border-radius: 50%;
+}
+
+</style>
